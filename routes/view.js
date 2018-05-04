68e508f9d67fd7094cf0b52e7197a193f4a86706
zhouwensheng <ex_zhouws>
2018-05-04 16:51:42 +0800

    dsfgdfsg

---
+++ routes/view.js
@@ -0,0 +1,23 @@
+// 电子书
+var express = require('express');
+var router = express.Router();
+var mysql = require('mysql');
+var connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '931017Zhouws',
+    port: '3306',
+    database: 'run111',
+})
+connection.connect();
+router.post('/videoList', function (req, res){
+    var sql = 'SELECT * FROM view';
+    connection.query(sql, function(err, result){
+        if(err){
+            console.log('[SELECT ERROR] - ', err.message);
+            return;
+        }
+        res.send({success:true,data:result})
+    })
+})
+module.exports = router;
